146b42e7e6f9ad8b866b56c660a133c60a028b6d
Alessio Sferro <sferro.alessio@gmail.com>
2024-04-15 19:23:03 +0200

    feat: add sprites and audio, add interactive sprites

--- js/game.js
+++ js/game.js
@@ -6,13 +6,76 @@ gameScene.init = function() {}
 
 // load asset files for our game
 gameScene.preload = function() {
+  this.load.image('background', '../assets/images/background-city.png');
+  this.load.image('building', '../assets/images/building.png');
+  this.load.image('car', '../assets/images/car.png');
+  this.load.image('house', '../assets/images/house.png');
+  this.load.image('tree', '../assets/images/tree.png');
 
+  this.load.audio('treeAudio', '../assets/audio/arbol.mp3');
+  this.load.audio('carAudio', '../assets/audio/auto.mp3');
+  this.load.audio('houseAudio', '../assets/audio/casa.mp3');
+  this.load.audio('correctAudio', '../assets/audio/correct.mp3');
+  this.load.audio('wrongAudio', '../assets/audio/wrong.mp3');
+  this.load.audio('buildingAudio', '../assets/audio/edificio.mp3');
 };
 
 // executed once, after assets were loaded
 gameScene.create = function() {
+  this.items = this.add.group([
+    {
+      key: 'building',
+      setXY: {
+        x: 140,
+        y: 230
+      }
+    },
+    {
+      key: 'car',
+      setXY: {
+        x: 270,
+        y: 290
+      },
+      setScale: {
+        x: .5,
+        y: .5
+      }
+    },
+    {
+      key: 'tree',
+      setXY: {
+        x: 370,
+        y: 255,
+      },
+      setScale: {
+        x: .8,
+        y: .8
+      }
+    },
+    {
+      key: 'house',
+      setXY: {
+        x: 500,
+        y: 270
+      },
+      setScale: {
+        x: .7,
+        y: .7
+      }
+    }
+  ]);
 
+  this.add.sprite(0, 0, 'background').setOrigin(0);
 
+  this.items.setDepth(1);
+
+  Phaser.Actions.Call(this.items.getChildren(), function(item) {
+    item.setInteractive();
+
+    item.on('pointerdown', function (pointer) {
+      console.log('You clicked ' + item.texture.key);
+    });
+  }, this);
 };
 
 // our game's configuration
